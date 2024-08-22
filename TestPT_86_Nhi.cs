eb3d2f69316eb6ea6d2b44200f74cf5feb39e7da
NguyenHoangNhi <109433578+NguyenHoangNhi@users.noreply.github.com>
2024-08-22 18:02:30 +0700

    Add files via upload

---
+++ TestPT_86_Nhi.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BTL_86_Nhi_N2;
+using System.Text;
+
+namespace GiaiPT2_Tester_86_Nhi
+{
+    [TestClass]
+    public class TestPT_86_Nhi
+    {
+        private PhuongTrinh_86_Nhi pt_86_Nhi;
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        public void TestVoNghiem_86_Nhi()
+        {
+            //Khai báo 2 biến kiểu string
+            string expected_86_Nhi, actual_86_Nhi;
+            //Khởi tạo một đối tưởng mới của lớp PhuongTrinh_86_Nhi và gán vào biến pt_86_Nhi
+            pt_86_Nhi = new PhuongTrinh_86_Nhi(2, 3, 4);
+            //Gán kết quả mong đợi vào biến expected_86_Nhi
+            expected_86_Nhi = "Phương trình vô nghiệm";
+            //Gọi phương thức GiaiPT_86_Nhi để giải phương trình sau khi đã truyền tham số và gán vào biến actual_86_Nhi
+            actual_86_Nhi = pt_86_Nhi.GiaiPT_86_Nhi();
+            //So sánh kết quả mong đợi và kết quả thực tế
+            Assert.AreEqual(expected_86_Nhi, actual_86_Nhi);
+        }
+        [TestMethod]
+        public void TestNghiemKep_86_Nhi()
+        {
+            //Khai báo 2 biến kiểu string
+            string expected_86_Nhi, actual_86_Nhi;
+            //Khởi tạo một đối tưởng mới của lớp PhuongTrinh_86_Nhi và gán vào biến pt_86_Nhi
+            pt_86_Nhi = new PhuongTrinh_86_Nhi(1, 2, 1);
+            //Gán kết quả mong đợi vào biến expected_86_Nhi
+            expected_86_Nhi = "Phương trình có nghiệm kép là:\r\nx1 = x2 = -1";
+            //Gọi phương thức GiaiPT_86_Nhi để giải phương trình sau khi đã truyền tham số và gán vào biến actual_86_Nhi
+            actual_86_Nhi = pt_86_Nhi.GiaiPT_86_Nhi();
+            //So sánh kết quả mong đợi và kết quả thực tế
+            Assert.AreEqual(expected_86_Nhi, actual_86_Nhi);
+        }
+        [TestMethod]
+        public void TestCoNghiem_86_Nhi()
+        {
+            //Khai báo 2 biến kiểu string
+            string expected_86_Nhi, actual_86_Nhi;
+            //Khởi tạo một đối tưởng mới của lớp PhuongTrinh_86_Nhi và gán vào biến pt_86_Nhi
+            pt_86_Nhi = new PhuongTrinh_86_Nhi(1, -5, 6);
+            //Gán kết quả mong đợi vào biến expected_86_Nhi
+            expected_86_Nhi = "Phương trình có 2 nghiệm phân biệt:\r\nx1 = 3\r\nx2 = 2";
+            //Gọi phương thức GiaiPT_86_Nhi để giải phương trình sau khi đã truyền tham số và gán vào biến actual_86_Nhi
+            actual_86_Nhi = pt_86_Nhi.GiaiPT_86_Nhi();
+            //So sánh kết quả mong đợi và kết quả thực tế
+            Assert.AreEqual(expected_86_Nhi, actual_86_Nhi);
+        }
+        //Nghiệm thuộc phương trình
+        [TestMethod]
+        public void TestNghiemThuoc_86_Nhi()
+        {
+            //Khai báo 2 biến kiểu string
+            string expected_86_Nhi, actual_86_Nhi;
+            //Khởi tạo một đối tưởng mới của lớp PhuongTrinh_86_Nhi và gán vào biến pt_86_Nhi
+            pt_86_Nhi = new PhuongTrinh_86_Nhi(1, -5, 6);
+            //Gán kết quả mong đợi vào biến expected_86_Nhi
+            expected_86_Nhi = 2 + " là nghiệm của phương trình";
+            //Gọi phương thức KiemTraX_86_Nhi truyền tham số 2 để kiểm tra và gán vào biến actual_86_Nhi
+            actual_86_Nhi = pt_86_Nhi.KiemTraX_86_Nhi(2);
+            //So sánh kết quả mong đợi và kết quả thực tế
+            Assert.AreEqual(expected_86_Nhi, actual_86_Nhi);
+        }
+        //Nghiệm không thuộc phương trình
+        [TestMethod]
+        public void TestNghiemKhongThuoc_86_Nhi()
+        {
+            //Khai báo 2 biến kiểu string
+            string expected_86_Nhi, actual_86_Nhi;
+            //Khởi tạo một đối tưởng mới của lớp PhuongTrinh_86_Nhi và gán vào biến pt_86_Nhi
+            pt_86_Nhi = new PhuongTrinh_86_Nhi(1, -5, 6);
+            //Gán kết quả mong đợi vào biến expected_86_Nhi
+            expected_86_Nhi = 1 + " không là nghiệm của phương trình";
+            //Gọi phương thức KiemTraX_86_Nhi truyền tham số 1 để kiểm tra và gán vào biến actual_86_Nhi
+            actual_86_Nhi = pt_86_Nhi.KiemTraX_86_Nhi(1);
+            //So sánh kết quả mong đợi và kết quả thực tế
+            Assert.AreEqual(expected_86_Nhi, actual_86_Nhi);
+        }
+        
+
+        //liên kết Testdata tìm nghiệm với project
+        [TestMethod]
+        //Dùng để chỉ định nguồn dữ liệu cho kiểm thử, nguồn dữ liệu được cung cấp từ một tệp CSV
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data_86_Nhi\TestDataTimNghiem_86_Nhi.csv", "TestDataTimNghiem_86_Nhi#csv", DataAccessMethod.Sequential)]
+        public void TestWithDataSource_TimNghiem_86_Nhi()
+        {
+            //Khai báo 3 biến kiểu int
+            int a_86_Nhi, b_86_Nhi, c_86_Nhi;
+            //Khai báo 2 biến kiểu string
+            string expected_86_Nhi, actual_86_Nhi;
+            //Lấy giá trị trong nguồn dữ liệu sau đó gán vào biến, giá trị được lấy ở cột đầu tiên
+            a_86_Nhi = int.Parse(TestContext.DataRow[0].ToString());
+            //Lấy giá trị trong nguồn dữ liệu sau đó gán vào biến, giá trị được lấy ở cột thứ 2
+            b_86_Nhi = int.Parse(TestContext.DataRow[1].ToString());
+            //Lấy giá trị trong nguồn dữ liệu sau đó gán vào biến, giá trị được lấy ở cột thứ 3
+            c_86_Nhi = int.Parse(TestContext.DataRow[2].ToString());
+            //Chuyển đổi dữ liệu từ một kiểu mã hóa sang kiểu chuỗi và gán vào biến expected_86_Nhi
+            expected_86_Nhi = Encoding.UTF8.GetString(Encoding.Default.GetBytes(TestContext.DataRow.ItemArray[3].ToString()));
+            //Khởi tạo một đối tượng mới của lớp PhuongTrinh_86_Nhi, sau đó gán vào biến pt_86_Nhi
+            pt_86_Nhi = new PhuongTrinh_86_Nhi(a_86_Nhi, b_86_Nhi, c_86_Nhi);
+            //Gọi phương thức GiaiPT_86_Nhi() để giải sau đó sẽ xóa các kí tự "\n" và "\r"
+            actual_86_Nhi = pt_86_Nhi.GiaiPT_86_Nhi().Replace("\n", "").Replace("\r", "");
+            //So sánh kết quả mong đợi với kết quả thực tế
+            Assert.AreEqual(expected_86_Nhi, actual_86_Nhi);
+        }
+
+        //liên kết Testdata kiểm tra nghiệm với project
+        [TestMethod]
+        //Dùng để chỉ định nguồn dữ liệu cho kiểm thử, nguồn dữ liệu được cung cấp từ một tệp CSV
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data_86_Nhi\TestDataKTNghiem_86_Nhi.csv", "TestDataKTNghiem_86_Nhi#csv", DataAccessMethod.Sequential)]
+        public void TestWithDataSource_KTNghiem_86_Nhi()
+        {
+            //Khai báo 4 biến kiểu int
+            int a_86_Nhi, b_86_Nhi, c_86_Nhi, x_86_Nhi;
+            //Khai báo 2 biến kiểu string
+            string expected_86_Nhi, actual_86_Nhi;
+            //Lấy giá trị trong nguồn dữ liệu sau đó gán vào biến, giá trị được lấy ở cột đầu tiên
+            a_86_Nhi = int.Parse(TestContext.DataRow[0].ToString());
+            //Lấy giá trị trong nguồn dữ liệu sau đó gán vào biến, giá trị được lấy ở cột thứ 2
+            b_86_Nhi = int.Parse(TestContext.DataRow[1].ToString());
+            //Lấy giá trị trong nguồn dữ liệu sau đó gán vào biến, giá trị được lấy ở cột thứ 3
+            c_86_Nhi = int.Parse(TestContext.DataRow[2].ToString());
+            //Lấy giá trị trong nguồn dữ liệu sau đó gán vào biến, giá trị được lấy ở cột thứ 4
+            x_86_Nhi = int.Parse(TestContext.DataRow[3].ToString());
+            //Chuyển đổi dữ liệu từ một kiểu mã hóa sang kiểu chuỗi và gán vào biến expected_86_Nhi
+            expected_86_Nhi = Encoding.UTF8.GetString(Encoding.Default.GetBytes(TestContext.DataRow.ItemArray[4].ToString()));
+            //Khởi tạo một đối tượng mới của lớp PhuongTrinh_86_Nhi, sau đó gán vào biến pt_86_Nhi
+            pt_86_Nhi = new PhuongTrinh_86_Nhi(a_86_Nhi, b_86_Nhi, c_86_Nhi);
+            //Gọi phương thức GiaiPT_86_Nhi() để giải và gán kết quả vào biến actual_86_Nhi
+            actual_86_Nhi = pt_86_Nhi.KiemTraX_86_Nhi(x_86_Nhi);
+            //So sánh kết quả mong đợi với kết quả thực tế
+            Assert.AreEqual(expected_86_Nhi, actual_86_Nhi);
+        }
+    }
+}
---
+++ TestWebBanHang_86_Nhi.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+
+namespace WebDriver_TestWebBanHang_86_Nhi
+{
+    [TestClass]
+    public class TestWebBanHang_86_Nhi
+    {
+        private ChromeDriverService chrome_86_Nhi;
+        private IWebDriver driver_86_Nhi;
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void Initialize_86_Nhi()
+        {
+            chrome_86_Nhi = ChromeDriverService.CreateDefaultService();
+            chrome_86_Nhi.HideCommandPromptWindow = true;
+            driver_86_Nhi = new ChromeDriver(chrome_86_Nhi);
+        }
+        //Chức năng 1: Đăng nhập
+        //liên kết Testdata đăng nhập với project
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data_LogIn_86_Nhi\TestDataLogIn_86_Nhi.csv", "TestDataLogIn_86_Nhi#csv", DataAccessMethod.Sequential)]
+        public void TC1_LogInSuccess_86_Nhi()
+        {
+            //khai báo biến kiểu chuỗi
+            string email_86_Nhi, pass_86_Nhi;
+            //Lấy giá trị cột đầu tiên của hàng hiện tại và gán vào biến email_86_Nhi
+            email_86_Nhi = TestContext.DataRow[0].ToString();
+            //Lấy giá trị cột thứ hai của hàng hiện tại và gán vào biến pass_86_Nhi
+            pass_86_Nhi = TestContext.DataRow[1].ToString();
+            //Vào trình duyệt, điều hướng đến URL
+            driver_86_Nhi.Navigate().GoToUrl("https://yody.vn/");
+            //Tìm phần tử có thẻ a là "ĐĂNG NHẬP", sau đó sử dụng phương thức Click()
+            driver_86_Nhi.FindElement(By.LinkText("ĐĂNG NHẬP")).Click();
+            //Tìm phần tử <input> có thuộc tính type='email' và nhập giá trị của biến email_86_Nhi
+            driver_86_Nhi.FindElement(By.CssSelector("input[type='email']")).SendKeys(email_86_Nhi);
+            //Dùng By.XPath để lấy dựa trên chuỗi XPath và nhập giá trị của biến pass_86_Nhi
+            driver_86_Nhi.FindElement(By.XPath("//*[@id=\"customer_password\"]")).SendKeys(pass_86_Nhi);
+            //Dùng By.ClassName để lấy phần tử class là "btn-login" sau đó dùng phương thức Click()
+            driver_86_Nhi.FindElement(By.ClassName("btn-login")).Click();
+        }
+
+        //Chức năng 2: Tìm kiếm sản phẩm và xem chi tiết
+        [TestMethod]
+        public void TC1_SearchInStore_86_Nhi()
+        {
+            //TC1: Tìm kiếm sản phẩm có trong cửa hàng
+            //Vào trình duyệt, điều hướng đến URL
+            driver_86_Nhi.Navigate().GoToUrl("https://yody.vn/");
+            //Dùng By.CssSelector để lấy ra phần tử có selector là "input[type='text']" và gửi giá trị "Quần"
+            driver_86_Nhi.FindElement(By.CssSelector("input[type='text']")).SendKeys("Quần");
+            // Dùng By.XPath để lấy dựa trên chuỗi XPath sau đó dùng phương thức Click()
+            driver_86_Nhi.FindElement(By.XPath("//*[@id=\"header-search-product\"]/button")).Click();
+            //Tìm phần tử có thẻ a là "Quần Jean Nam Coolmax Mác Logo Đỉa Quần", sau đó sử dụng phương thức Click()
+            driver_86_Nhi.FindElement(By.LinkText("Quần Jean Nam Coolmax Mác Logo Đỉa Quần")).Click();
+        }
+
+        [TestMethod]
+        public void TC2_SearchOutStore_86_Nhi()
+        {
+            //TC2: Tìm kiếm sản phẩm không có trong cửa hàng
+            //Vào trình duyệt, điều hướng đến URL
+            driver_86_Nhi.Navigate().GoToUrl("https://yody.vn/");
+            //Dùng By.Name để lấy phần tử có name là "query", sau đó gửi giá trị "Xe máy" lên
+            driver_86_Nhi.FindElement(By.Name("query")).SendKeys("Xe máy");
+            //Dùng By.XPath để lấy dựa trên chuỗi XPath sau đó dùng phương thức Click()
+            driver_86_Nhi.FindElement(By.XPath("//*[@id=\"header-search-product\"]/button")).Click();
+        }
+
+        //Chức năng 3: Thêm sản phẩm vào giỏ hàng
+        [TestMethod]
+        public void TC1_AddSuccess_86_Nhi()
+        {
+            //TC1: Thêm sản phẩm thành công
+            //Vào trình duyệt, và điều hướng đến URL
+            driver_86_Nhi.Navigate().GoToUrl("https://yody.vn/quan-jean-nam-coolmax-mac-logo-dia-quan");
+            //Khởi tạo một đối tượng Actions để thực hiện thao tác với trình duyệt
+            Actions actions_86_Nhi = new Actions(driver_86_Nhi);
+            //Tìm phần tử trên trang có thuộc tính id là "swatch-1-30" và gán vào biến element_86_Nhi
+            IWebElement element_86_Nhi = driver_86_Nhi.FindElement(By.Id("swatch-1-30"));
+            //Di chuyển chuột đến phần tử element_86_Nhi sau đó thực hiện thao tác nhấp chuột.
+            actions_86_Nhi.MoveToElement(element_86_Nhi).Click().Perform();
+            //Tìm phần tử trên trang bằng cách sử dụng XPath là "/html/body/div[7]/div/div[2]/button" và gán vào biến submitElement_86_Nhi
+            IWebElement submitElement_86_Nhi = driver_86_Nhi.FindElement(By.XPath("/html/body/div[7]/div/div[2]/button"));
+            //Di chuyển chuột đến phần tử submitElement_86_Nhi sau đó thực hiện thao tác nhấp chuột.
+            actions_86_Nhi.MoveToElement(submitElement_86_Nhi).Click().Perform();
+        }
+
+        [TestMethod]
+        public void TC2_AddFail_86_Nhi()
+        {
+            //TC2: Thêm sản phẩm không thành công
+            //Vào trình duyệt, và điều hướng đến địa chỉ URL
+            driver_86_Nhi.Navigate().GoToUrl("https://yody.vn/giay-dep-cao-got-quai-ngang");
+            //Khởi tạo một đối tượng Actions để thực hiện thao tác với trình duyệt
+            Actions actions_86_Nhi = new Actions(driver_86_Nhi);
+            //Tìm phần tử trên trang bằng cách sử dụng XPath là "/html/body/section/div[4]/div[1]/div[2]/div/form/div/div[1]/div[1]/div[1]/div[4]/label/span" và gán vào biến element_86_Nhi
+            IWebElement element_86_Nhi = driver_86_Nhi.FindElement(By.XPath("/html/body/section/div[4]/div[1]/div[2]/div/form/div/div[1]/div[1]/div[1]/div[4]/label/span"));
+            //Di chuyển chuột đến phần tử element_86_Nhi sau đó thực hiện thao tác nhấp chuột.
+            actions_86_Nhi.MoveToElement(element_86_Nhi).Click().Perform();
+            //Dùng By.Id để lấy phần tử có id là "swatch-1-39"
+            IWebElement sizeElement_86_Nhi = driver_86_Nhi.FindElement(By.Id("swatch-1-39"));
+            if (sizeElement_86_Nhi.Selected)
+            {
+                //Tìm phần tử trên trang bằng cách sử dụng ClassName là "add-span-cart" và gán vào biến submitElement_86_Nhi
+                IWebElement submitElement_86_Nhi = driver_86_Nhi.FindElement(By.ClassName("add-span-cart"));
+                //Di chuyển chuột đến ohaanf tử submitElement_86_Nhi sau đó thực hiện thao tác nhấp chuột.
+                actions_86_Nhi.MoveToElement(submitElement_86_Nhi).Click().Perform();
+            }
+            else
+            {
+                //Nếu như kích thước chưa được chọn thì sẽ xuất ra dòng để thông báo
+                Console.WriteLine("Lỗi: Bạn chưa chọn kích thước sản phẩm");
+            }
+        }
+
+        /* [TestCleanup]
+         public void Cleanup()
+         {
+             driver_86_Nhi.Quit();
+         }*/
+    }
+}
